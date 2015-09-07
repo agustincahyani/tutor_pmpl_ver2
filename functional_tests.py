ff4e5c9ad3b4616528aa072e4389fa50268205e2
agustincahyani <agustincahyani29@gmail.com>
2015-09-07 06:20:20 +0000

    Modifying the view and the test

--- functional_tests.py
+++ functional_tests.py
@@ -12,11 +12,12 @@ class NewVisitorTest(unittest.TestCase): #1
 	def test_can_start_a_list_and_retrieve_it_later(self): #4
 		# Edith has heard about a cool new online to-do app
 		# She goes to check out its homepage
-		self.browser.get('http://localhost:8000')
+		self.browser.get('http://localhost')
 
 		# She notices the page title and header mention to-do lists
-		self.assertIn('To-Do', self.browser.title) #5
-		self.fail('Finish the test') #6
+		self.assertIn('Second Django App', self.browser.title) #5
+#		self.fail('Finish the test') #6
+		self.assertIn('Dhika Ayu Agustin Cahyani', self.browser.find_element_by_tag_name('body').text)
 
 		# She is invited to enter a to-do item straight away
 
