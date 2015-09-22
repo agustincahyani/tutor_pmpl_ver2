d1e3b2a2525f85e982e2ca62b934b9f71ba471a2
agustincahyani <agustincahyani29@gmail.com>
2015-09-22 14:40:12 +0000

    fixing urls

--- superlists/urls.py
+++ superlists/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import include, url
-from lists import urls as list_urls
 from lists import views as list_views
+from lists import urls as list_urls
 
 urlpatterns = [
     url(r'^$', list_views.home_page, name='home'),
