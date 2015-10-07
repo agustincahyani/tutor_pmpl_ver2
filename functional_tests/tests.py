fd8f060c8c9dd24d1afe1235c656dd72cd145534
agustincahyani <agustincahyani29@gmail.com>
2015-10-07 10:32:10 +0000

    Hack FT runner to be able to test staging

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,9 +1,24 @@
+import sys
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(StaticLiveServerTestCase): #1
+
+	@classmethod
+	def setUpClass(cls):
+		for arg in sys.argv:
+			if 'liveserver' in arg:
+				cls.server_url = 'http://' + arg.split('=')[1]
+				return
+		super().setUpClass()
+		cls.server_url = cls.live_server_url
+
+	@classmethod
+	def tearDownClass(cls):
+		if cls.server_url == cls.live_server_url:
+			super().tearDownClass()
 
 	def setUp(self): #2
 		self.browser = webdriver.Firefox()
@@ -19,7 +34,7 @@ class NewVisitorTest(StaticLiveServerTestCase): #1
 	def test_can_start_a_list_and_retrieve_it_later(self): #4
 		# Edith has heard about a cool new online to-do app
 		# She goes to check out its homepage
-		self.browser.get(self.live_server_url)
+		self.browser.get(self.server_url)
 
 		# She notices the page title and header mention to-do lists
 		self.assertIn('To-Do', self.browser.title) #5
@@ -57,7 +72,7 @@ class NewVisitorTest(StaticLiveServerTestCase): #1
 		self.browser = webdriver.Firefox()
 
 		# Francis visits the home page. There is no sign of Edith's list
-		self.browser.get(self.live_server_url)
+		self.browser.get(self.server_url)
 		page_text = self.browser.find_element_by_tag_name('body').text
 		self.assertNotIn('Buy peacock feathers', page_text)
 		self.assertNotIn('make a fly', page_text)
@@ -80,7 +95,7 @@ class NewVisitorTest(StaticLiveServerTestCase): #1
 
 	def test_layout_and_styling(self):
 		# Edith goes to the home page
-		self.browser.get(self.live_server_url)
+		self.browser.get(self.server_url)
 		self.browser.set_window_size(1024, 768)
 
 		# she notices the input inbox is nicely centered
@@ -104,7 +119,7 @@ class NewVisitorTest(StaticLiveServerTestCase): #1
 		tablebox = self.browser.find_element_by_id('id_list_table')
 		self.assertAlmostEqual(
 			tablebox.location['x'] + tablebox.size['width'] / 2,
-			755,
+			512,
 			delta=5
 		)
 if __name__ == '__main__': #7
