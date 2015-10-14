dc015e57fbc173bdf7ee3fb847b0d29b145c8346
agustincahyani <agustincahyani29@gmail.com>
2015-10-14 09:01:49 +0000

    Adding functional test for blog

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -122,5 +122,15 @@ class NewVisitorTest(StaticLiveServerTestCase): #1
 			512,
 			delta=5
 		)
+
+	def test_for_blogging_address(self):
+		self.browser.get(self.server_url)
+		blogging_box = self.browser.find_element_by_id('id_blog_address')
+		self.assertIn('Please visit my blog at this address', blogging_box.text)
+
+	def test_for_blogging_page(self):
+		self.browser.get(self.server_url+"/blog/")
+		self.assertIn('My Personal Blog', self.browser.title)
+
 if __name__ == '__main__': #7
 	unittest.main(warnings='ignore') #8
