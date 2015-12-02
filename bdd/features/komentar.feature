8fdb240cd44a2ae535a5bb0ddadaa69100c42f46
agustincahyani <agustincahyani29@gmail.com>
2015-12-02 10:55:13 +0000

    edit komentar.py

--- bdd/features/komentar.feature
+++ bdd/features/komentar.feature
@@ -3,11 +3,11 @@ Feature: Provide automatic comment base on the number of items
 	I want to have an automatic comment base on the number tasks
 	So I can know how busy i'm
 
-Scenarion Outline: opening page
+Scenario Outline: opening page
 	Given I am opening the web page
 	When I see <numberofitems> item's in my to do list
 	Then I see a <comment> about how busy i'm
-	Example: comments
+	Examples:
 		|	numberofitems	| comment 					|
 		|	0				| "yey, waktunya berlibur" 	|
 		|	3				| "sibuk tapi santai"		|
---
+++ bdd/features/steps/komentar.py
@@ -0,0 +1,29 @@
+from behave import *
+
+@when("I see 0 item's in my to do list")
+def impl(context):
+	assert False
+
+@then("I see a \"sibuk tapi santai\" about how busy i'm")
+def impl(context):
+	assert False
+
+@then("I see a \"yey, waktunya berlibur\" about how busy i'm")
+def impl(context):
+	assert False
+
+@when("I see 3 item's in my to do list")
+def impl(context):
+	assert False
+
+@when("I see 5 item's in my to do list")
+def impl(context):
+	assert False
+
+@given('I am opening the web page')
+def impl(context):
+	assert False
+
+@then("I see a \"oh tidak\" about how busy i'm")
+def impl(context):
+	assert False
